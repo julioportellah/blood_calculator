98dfa2bc1a4be352f85931679830c3ab31f6dfd4
julioportellah <julioportellah@gmail.com>
2020-08-24 09:42:09 -0700

    Adding loop to ensure that the quit button works properly

--- calculator.py
+++ calculator.py
@@ -1,9 +1,13 @@
 def interface():
     print('MY Program')
-    print('Options:')
-    print('9- Quit')
-    choice=input("Enter your code: ")
-    if choice=='9':
-        return
+    while True:
+        print('Options:')
+        print('9- Quit')
+        choice=input("Enter your code: ")
+        if choice=='9':
+            return
+        
+        pass
+    
 
 interface()
