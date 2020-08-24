2aa57588048703e1fb71d616f929c3d03139b3a5
julioportellah <julioportellah@gmail.com>
2020-08-24 09:59:14 -0700

    add an HDL option to main interface

--- calculator.py
+++ calculator.py
@@ -2,12 +2,14 @@ def interface():
     print('MY Program')
     while True:
         print('Options for you:')
+        print('1- HDL')
         print('9- Quit')
         choice=input("Enter your code: ")
         if choice=='9':
             return
-        
+        elif choice =='1':
+            HDL_driver()
         pass
-    
+   
 
 interface()
